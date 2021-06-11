7e7acb5d579efbbfcd3d5e741f2eb39965cfff62
phuongduyphan <phuongduyphan@gmail.com>
2021-06-11 11:06:45 +0700

    Add hash table

--- src/c/memory.c
+++ src/c/memory.c
@@ -14,24 +14,25 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
   return result;
 }
 
-void freeObjects() {
-  Obj* object = vm.objects;
-  while (object != NULL) {
-    Obj* next = object->next;
-    freeObj(object);
-    object = next;
-  }
-}
-
 static void freeObject(Obj* object) {
   switch (object->type) {
-    case OBJ_STRING:
+    case OBJ_STRING: {
       ObjString* string = (ObjString*)object;
       FREE_ARRAY(char, string->chars, string->length + 1);
       FREE(ObjString, object);
       break;
+    }
     
     default:
       break;
+  }
+}
+
+void freeObjects() {
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    Obj* next = object->next;
+    freeObject(object);
+    object = next;
   }
 }
